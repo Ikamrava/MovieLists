d2aac3e332a7b76cdd291e1a1cb5ad9effc4bdbc
ikamrava <ikamrava>
2022-11-27 20:11:26 +0000

    search

--- java.js
+++ java.js
@@ -1,59 +1,73 @@
 const container = document.getElementById("container")
+const input = document.getElementById("input")
+const notfind = document.getElementById("notfound")
 
-async function searchData(){
+document.getElementById("search").addEventListener("click",function(){
+
+    (async () => {
+        if(await searchData(input.value)){
+            container.innerHTML = await searchData(input.value)
+        }else{
+            notfind.innerHTML = `Unable to find what you're looking for.Please try another search`
+        }
+        
+            
+    })()
+})   
+
+  async function searchData(userinput){
     let generatelist = []
     let idList = []
     html = ""
-    const res = await fetch("http://www.omdbapi.com/?s=Blade runner&apikey=d868482e")
-    const data = await res.json()
-    for (item of data.Search){
-        idList.push(item.imdbID)  
-    }
-    for (id of idList){
-        const resBasedID = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=d868482e`)
-        const basedID = await resBasedID.json()
-        generatelist.push(basedID)
-        if (basedID.Poster!= "N/A"){
-
-        html += ` 
-                    <div class="itemWarpper">
-                                
-                        <div class="mainimage">
-                            <img id ="mainimg"src="${basedID.Poster}" alt="Poster">
-                        </div>                             
-                        <div class="informations">
-                            <div class="title">
-                                <h1 id="title">${basedID.Title}</h1>
-                                <div><img src="star.png" alt="star"></div>
-                                
-                                <p id="rating">${basedID.imdbRating}</p>
-                            </div>
-                            <div class="info">
-                                <p id="runtime">${basedID.Runtime}</p>
-                                <p id = "genre">${basedID.Genre}</p>
-                                <div class="watchlist">
-                                    <img src="add.png" alt="">
-                                    <p>Watchlist</p>
-                                </div>
-                            </div>
-                            <p id="plot">${basedID.Plot}</p>
-
-                        </div>
-                    </div>`
+    try{
+        const res = await fetch(`http://www.omdbapi.com/?s=${userinput}&apikey=d868482e`)
+        const data = await res.json()
+        for (item of data.Search){
+            idList.push(item.imdbID)  
         }
-       
+        for (id of idList){
+            const resBasedID = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=d868482e`)
+            const basedID = await resBasedID.json()
+            generatelist.push(basedID)
+            if (basedID.Poster!= "N/A"){
+            html += ` 
+                        <div class="itemWarpper">
+                                    
+                            <div class="mainimage">
+                                <img id ="mainimg"src="${basedID.Poster}" alt="Poster">
+                            </div>                             
+                            <div class="informations">
+                                <div class="title">
+                                    <h1 id="title">${basedID.Title}</h1>
+                                    <div><img src="star.png" alt="star"></div>
+                                    
+                                    <p id="rating">${basedID.imdbRating}</p>
+                                </div>
+                                <div class="info">
+                                    <p id="runtime">${basedID.Runtime}</p>
+                                    <p id = "genre">${basedID.Genre}</p>
+                                    <div class="watchlist">
+                                        <img src="add.png" alt="">
+                                        <p>Watchlist</p>
+                                    </div>
+                                </div>
+                                <p id="plot">${basedID.Plot}</p>
+    
+                            </div>
+                        </div>`
+            }
+           
+        }
+        
+    return html
+    
+
+    }catch(error){
+        
+
     }
-return html
+    
+   
+    
 
 }
-
-(async () => {
-    console.log(await searchData())
-    container.innerHTML = (await searchData())
-    
-  })()
-
-
-  
-// const resId = await fetch(`http://www.omdbapi.com/?i=${item.imdbID}&apikey=d868482e`)
-// const dataBasedOnID = resId.json()
